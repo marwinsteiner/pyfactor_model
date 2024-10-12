5c2620e0b5c5a2918425f7d0f2db66949d4c2fa2
Marwin Steiner <marwin.steiner@gmail.com>
2024-10-12 12:56:38 +0100

    refactor market cap weight portfolio construction.

--- src/utils/portfolio_construction.py
+++ src/utils/portfolio_construction.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from typing import Dict
-from src.utils import data_preparation
+from src.utils.data_preparation import load_snp_constituents, prepare_returns_data
 
 
 def construct_equal_weight_portfolio(returns_data: pd.DataFrame) -> pd.Series:
@@ -11,9 +11,14 @@ def construct_equal_weight_portfolio(returns_data: pd.DataFrame) -> pd.Series:
 
 
 def construct_market_cap_weight_portfolio(historical_data: Dict[str, pd.DataFrame]) -> pd.Series:
-    """Construct a market-cap weighted portfolio."""
-    market_caps = pd.Series({ticker: data['close'].iloc[-1] * data['volume'].iloc[-1]
-                             for ticker, data in historical_data.items()})
-    weights = market_caps / market_caps.sum()
-    returns_data = data_preparation.prepare_returns_data(historical_data)
-    return (returns_data * weights).sum(axis=1)
+    """Construct a market-cap weighted portfolio using S&P 500 weights."""
+    returns_data = prepare_returns_data(historical_data)
+    snp_weights = load_snp_constituents()
+
+    # Filter weights for stocks present in our historical data
+    portfolio_weights = snp_weights[snp_weights.index.isin(returns_data.columns)]
+
+    # Normalize weights to sum to 1
+    portfolio_weights = portfolio_weights / portfolio_weights.sum()
+
+    return (returns_data * portfolio_weights).sum(axis=1)
